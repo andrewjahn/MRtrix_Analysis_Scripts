2b208b00597edb161695accf52239ce0778e3d03
andrewjahn <48071253+andrewjahn@users.noreply.github.com>
2019-02-28 15:39:35 -0500

    Update updated_MRtrix_Preproc.sh

--- updated_MRtrix_Preproc.sh
+++ updated_MRtrix_Preproc.sh
@@ -2,6 +2,7 @@
 
 # Written by Andrew Jahn, University of Michigan, 02.25.2019
 # Based on Marlene Tahedl's BATMAN tutorial (http://www.miccai.org/edu/finalists/BATMAN_trimmed_tutorial.pdf)
+# Thanks to John Plass and Bennet Fauber for useful comments
 
 display_usage() {
 	echo "$(basename $0) [Raw Diffusion] [Fieldmap] [AP bvec] [AP bval] [PA bvec] [PA bval] [Anatomical]"
@@ -31,33 +32,35 @@ ANAT=$7
 
 
 ########################### STEP 1 ###################################
-# Convert data to .mif format, denoise, and perform Gibbs de-ringing #
+#	        Convert data to .mif format and denoise	   	     #
 ######################################################################
 
+# Also consider doing Gibbs denoising (using mrdegibbs). Check your diffusion data for ringing artifacts before deciding whether to use it
 mrconvert $RAW_DWI raw_dwi.mif -fslgrad $BVEC $BVAL
 dwidenoise raw_dwi.mif dwi_den.mif -noise noise.mif
-mrcalc raw_dwi.mif dwi_den.mif -subtract residual.mif
-mrdegibbs dwi_den.mif dwi_den_unr.mif -axes 0,1
 
 # Extract the b0 images from the diffusion data acquired in the AP direction
-dwiextract dwi_den_unr.mif - -bzero | mrmath - mean mean_b0_AP.mif -axis 3
+dwiextract dwi_den.mif - -bzero | mrmath - mean mean_b0_PA.mif -axis 3
 
-# Extracts the b0 images for diffusion data acquired in the PA direction
+# Extracts the b0 images for diffusion data acquired in the AP direction
 # In the fieldmap image, there are 8 images in this order: AP, PA, AP, PA, AP, PA, AP, PA. Indexing starts at 0
-mrconvert $FIELDMAP -coord 3 1 PA.mif
-mrconvert PA.mif -fslgrad $PA_BVEC $PA_BVAL - | mrmath - mean mean_b0_PA.mif -axis 3
+# For the AP_BVEC and AP_BVAL files, they should be in the follwing format (assuming you extract only one volume):
+# AP_BVEC: 0 0 0
+# AP_BVAL: 0
+mrconvert $FIELDMAP -coord 3 0 AP.mif
+mrconvert AP.mif -fslgrad $AP_BVEC $AP_BVAL - | mrmath - mean mean_b0_PA.mif -axis 3
 
 # Concatenates the b0 images from AP and PA directions to create a paired b0 image
-mrcat mean_b0_AP.mif mean_b0_PA.mif -axis 3 b0_pair.mif
+mrcat mean_b0_PA.mif mean_b0_AP.mif -axis 3 b0_pair.mif
 
-# Runs the dwipreproc command, which is a wrapper for eddy and topup. This step takes about 2 hours
-dwipreproc dwi_den_unr.mif dwi_den_unr_preproc.mif -pe_dir AP -rpe_pair -se_epi b0_pair.mif -eddy_options " --slm=linear --data_is_shelled"
+# Runs the dwipreproc command, which is a wrapper for eddy and topup. This step takes about 2 hours on an iMac desktop with 8 cores
+dwipreproc dwi_den.mif dwi_den_preproc.mif -pe_dir PA -rpe_pair -se_epi b0_pair.mif -eddy_options " --slm=linear --data_is_shelled"
 
 # Performs bias field correction. Needs ANTs to be installed in order to use the "ants" option (use "fsl" otherwise)
-dwibiascorrect -ants dwi_den_unr_preproc.mif dwi_den_unr_preproc_unbiased.mif -bias bias.mif
+dwibiascorrect -ants dwi_den_preproc.mif dwi_den_preproc_unbiased.mif -bias bias.mif
 
 # Create a mask for future processing steps
-dwi2mask dwi_den_unr_preproc_unbiased.mif mask.mif
+dwi2mask dwi_den_preproc_unbiased.mif mask.mif
 
 
 ########################### STEP 2 ###################################
@@ -65,10 +68,10 @@ dwi2mask dwi_den_unr_preproc_unbiased.mif mask.mif
 ######################################################################
 
 # Create a basis function from the subject's DWI data. The "dhollander" function is best used for multi-shell acquisitions; it will estimate different basis functions for each tissue type. For single-shell acquisition, use the "tournier" function instead
-dwi2response dhollander dwi_den_unr_preproc_unbiased.mif wm.txt gm.txt csf.txt -voxels voxels.mif
+dwi2response dhollander dwi_den_preproc_unbiased.mif wm.txt gm.txt csf.txt -voxels voxels.mif
 
 # Performs multishell-multitissue constrained spherical deconvolution, using the basis functions estimated above
-dwi2fod msmt_csd dwi_den_unr_preproc_unbiased.mif -mask mask.mif wm.txt wmfod.mif gm.txt gmfod.mif csf.txt csffod.mif
+dwi2fod msmt_csd dwi_den_preproc_unbiased.mif -mask mask.mif wm.txt wmfod.mif gm.txt gmfod.mif csf.txt csffod.mif
 
 # Creates an image of the fiber orientation densities overlaid onto the estimated tissues (Blue=WM; Green=GM; Red=CSF)
 # You should see FOD's mostly within the white matter. These can be viewed later with the command "mrview vf.mif -odf.load_sh wmfod.mif"
@@ -87,7 +90,7 @@ mrconvert $ANAT anat.mif
 5ttgen fsl anat.mif 5tt_nocoreg.mif
 
 # The following series of commands will take the average of the b0 images (which have the best contrast), convert them and the 5tt image to NIFTI format, and use it for coregistration.
-dwiextract dwi_den_unr_preproc_unbiased.mif - -bzero | mrmath - mean mean_b0_processed.mif -axis 3
+dwiextract dwi_den_preproc_unbiased.mif - -bzero | mrmath - mean mean_b0_processed.mif -axis 3
 mrconvert mean_b0_processed.mif mean_b0_processed.nii.gz
 mrconvert 5tt_nocoreg.mif 5tt_nocoreg.nii.gz
 
@@ -107,11 +110,10 @@ mrtransform 5tt_nocoreg.mif -linear diff2struct_mrtrix.txt -inverse 5tt_coreg.mi
 # Create streamlines
 # Note that the "right" number of streamlines is still up for debate. Last I read from the MRtrix documentation,
 # They recommend about 100 million tracks. Here I use 10 million, if only to save time. Read their papers and then make a decision
-tckgen -act 5tt_coreg.mif -backtrack -seed_gmwmi gmwmSeed_coreg.mif -select 10000k wmfod_norm.mif tracks_10mio.tck
+tckgen -act 5tt_coreg.mif -backtrack -seed_dynamic gmwmSeed_coreg.mif -nthreads 8 -maxlength 250 -cutoff 0.06 -select 10000000 wmfod_norm.mif tracks_10M.tck
 
 # Extract a subset of tracks (here, 200 thousand) for ease of visualization
-tckedit tracks_10mio.tck -number 200k smallerTracks_200k.tck
+tckedit tracks_10M.tck -number 200k smallerTracks_200k.tck
 
 # Reduce the number of streamlines with tcksift
-tcksift -act 5tt_coreg.mif -term_number 1000000 tracks_10mio.tck wmfod_norm.mif sift_1mio.tck
-
+tcksift2 -act 5tt_coreg.mif -out_mu sift_mu.txt -out_coeffs sift_coeffs.txt -nthreads 8 tracks_10M.tck wmfod_norm.mif sift_1M.tck
