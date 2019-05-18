f7ffdf3c5c9378be0577b9eb2f4e94e9e98c6321
andrewjahn <48071253+andrewjahn@users.noreply.github.com>
2019-05-18 17:25:18 -0400

    Update QC_mrview.sh

--- QC_mrview.sh
+++ QC_mrview.sh
@@ -7,7 +7,7 @@
 
 # Views the voxels used for FOD estimation
 echo "Now viewing the voxels used for FOD estimation (Blue=WM; Green=GM; Red=CSF)"
-mrview dwi_den_unr_preproc_unbiased.mif -overlay.load voxels.mif
+mrview dwi_den_preproc_unbiased.mif -overlay.load voxels.mif
 
 # Views the response functions for each tissue type. The WM function should flatten out at higher b-values, while the other tissues should remain spherical
 echo "Now viewing response function for white matter (press right arrow key to view response function for different shells)"
@@ -24,17 +24,17 @@ mrview vf.mif -odf.load_sh wmfod.mif
 ### Quality checks for Step 3 ###
 
 # Check alignment of the 5 tissue types before and after alignment (new alignment in red, old alignment in blue)
-mrview dwi_den_unr_preproc_unbiased.mif -overlay.load 5tt_nocoreg.mif -overlay.colourmap 2 -overlay.load 5tt_coreg.mif -overlay.colourmap 1
+mrview dwi_den_preproc_unbiased.mif -overlay.load 5tt_nocoreg.mif -overlay.colourmap 2 -overlay.load 5tt_coreg.mif -overlay.colourmap 1
 
 # Check the seed region (should match up along the GM/WM boundary)
-mrview dwi_den_unr_preproc_unbiased.mif -overlay.load gmwmSeed_coreg.mif
+mrview dwi_den_preproc_unbiased.mif -overlay.load gmwmSeed_coreg.mif
 
 
 ### Quality checks for Step 4 ###
 
 # View the tracks in mrview
-mrview dwi_den_unr_preproc_unbiased.mif -tractography.load smallerTracks_200k.tck
+mrview dwi_den_preproc_unbiased.mif -tractography.load smallerTracks_200k.tck
 
 # View the sifted tracks in mrview
-mrview dwi_den_unr_preproc_unbiased.mif -tractography.load sift_1mio.tck
+mrview dwi_den_preproc_unbiased.mif -tractography.load sift_1mio.tck
 
