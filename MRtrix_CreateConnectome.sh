921d05f148f9e10d14590512efab8e2786ba1dbd
Andrew Jahn <48071253+andrewjahn@users.noreply.github.com>
2019-07-16 14:58:39 -0400

    Update MRtrix_CreateConnectome.sh

--- MRtrix_CreateConnectome.sh
+++ MRtrix_CreateConnectome.sh
@@ -4,6 +4,10 @@
 #Convert the labels of the FreeSurfer parcellation to a format that MRtrix understands. This requires recon-all to have been run on the subject
 labelconvert -force sub-01_MRtrix/mri/aparc+aseg.mgz $FREESURFER_HOME/FreeSurferColorLUT.txt ~/mrtrix3/share/mrtrix3/labelconvert/fs_default.txt sub-01_parcels.mif
 
+# Add a line here for mrtransform to register the parcels to the diffusion data?
+
+# mrtransform sub-01_parcels.mif -interp nearest –linear diff2struct_mrtrix.txt –inverse –datatype uint32 sub-01_parcels_coreg.mif
+
 #Create a whole-brain connectome, representing the streamlines between each parcellation pair in the atlas (in this case, 84x84). The "symmetric" option will make the lower diagonal the same as the upper diagonal, and the "scale_invnodevol" option will scale the connectome by the inverse of the size of the node 
 tck2connectome -symmetric -zero_diagonal -scale_invnodevol -tck_weights_in sift_1M.txt sub-01_parcels.mif sub-01_parcels.csv -out_assignment assignments_sub-01_parcels.csv
 
